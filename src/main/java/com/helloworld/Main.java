ece0b03f45880db9c54d64949fd3c6d116bb668f
JuanPedroGallardo <juanpedro.gallardo@babelgroup.com>
2025-03-10 11:32:46 +0100

    Modificado para añadir un array de personas

--- src/main/java/com/helloworld/Main.java
+++ src/main/java/com/helloworld/Main.java
@@ -4,18 +4,24 @@ import com.google.gson.Gson;
 
 
 public class Main {
-    public static Persona conviertePersona(String json) {
+    public static Persona[] conviertePersona(String json) {
         Gson gson = new Gson();
-        return gson.fromJson(json, Persona.class);
+        return gson.fromJson(json, Persona[].class);
     }
 
     public static void main(String[] args) {
-        String json = "{\"nombre\": \"Juan Pedro\", \"edad\": 22}";
+        String json = "[" + "{\"nombre\": \"Juan Pedro\", \"edad\": 22}, " +
+                "{\"nombre\": \"Pablo\", \"edad\": 2}, " +
+                "{\"nombre\": \"Juan\", \"edad\": 22}" + "]";
 
         //Convertir el json a persona
-        Persona persona = conviertePersona(json);
+        Persona[] personas = conviertePersona(json);
 
-        System.out.println("Nombre: " + persona.getNombre());
-        System.out.println("Edad: " + persona.getEdad());
+        for (Persona persona : personas) {
+            System.out.println("Nombre: " + persona.getNombre());
+            System.out.println("Edad: " + persona.getEdad());
+        }
+
+
     }
 }
